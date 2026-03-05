7ffa3ef6d83dcb78bf5c74cef8ca4f2b3a4fcd5f
gantian127 <jamy127@foxmail.com>
2026-03-04 19:26:11 -0700

    update check_mass_balance.py

--- mpi_landlab/check_mass_balance.py
+++ mpi_landlab/check_mass_balance.py
@@ -12,22 +12,34 @@ this needs to export the final results from global grid as .npy file
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.use("agg")
 
 rank = 3
-shape = [6, 6]
-true_file_dir = os.path.join(os.getcwd(), "mpi_landlab","linear_diffusion_output_png_1")
-file_dir = os.path.join(os.getcwd(), "mpi_landlab",f"linear_diffusion_output_png_{rank}")
+shape = (17, 17)
+true_file_dir = os.path.join(
+    os.getcwd(), "mpi_landlab", "linear_diffusion_output_png_1"
+)
+file_dir = os.path.join(
+    os.getcwd(), "mpi_landlab", f"linear_diffusion_output_png_{rank}"
+)
 
 ## check elevation difference
 simulation_elev = np.load(os.path.join(file_dir, f"elevation_result_{rank}.npy"))
 true_elev = np.load(os.path.join(true_file_dir, "elevation_result_1.npy"))
 
-np.all(simulation_elev==true_elev)
-
 diff = simulation_elev - true_elev
 diff.reshape(shape)
 
+print(f"rank {rank} == rank {1}: {np.all(simulation_elev==true_elev)}")
+print(f"diff == 0: {np.all(diff==0)}")
+
 plt.imshow(diff.reshape(shape))
-plt.savefig(os.path.join(file_dir,"compare_final_elev_diff.png" ))
+plt.savefig(os.path.join(file_dir, f"elevation_diff_{rank}.png"))
 
+plt.imshow(simulation_elev.reshape(shape))
+plt.savefig(os.path.join(file_dir, f"elevation_final_{rank}.png"))
 
+plt.imshow(true_elev.reshape(shape))
+plt.savefig(os.path.join(true_file_dir, f"elevation_final_true.png"))
